4e80b2ab2a4cd9884de4968c86fd64f2a92afb28
chriskoh <chris.koh90@gmail.com>
2014-02-15 18:11:17 -0800

    Updated main program

--- jcomp.py
+++ jcomp.py
@@ -1,18 +1,25 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import json
 import pprint
 
 def get_json():
-    file_name = raw_input("Enter name of JSON File: ")
+    file_name = input("Enter name of JSON File: ")
     with open(file_name) as json_file:
         json_data = json.load(json_file)
         return json_data
 
-#def list_comp(json1, json2):
-    
+def list_diff(json1, json2):
+    for i in json1:
+        if i in json2:
+            print('     "' + str(i) + '"')
+        elif i not in json2:
+            print('-    "' + str(i) + '"')
+    for i in json2:
+        if i not in json1:
+            print('+    "' + str(i) + '"')
 
-def print_diff(json1, json2):
+def dict_diff(json1, json2):
     for n in json1:
         if n not in json2:
             print('-   "' + str(n) + '":')
@@ -27,22 +34,35 @@ def print_diff(json1, json2):
             else:
                 if type(json2[n]) == dict:
                     print('    "' + str(n) + '" : {')
-                    print_diff(json1[n], json2[n])
+                    dict_diff(json1[n], json2[n])
                     continue
         else:
             if type(json1[n]) == dict:
-                json1[n].pop('{',0)
                 print('    "' + str(n) + '" : {')
                 pprint.pprint(json1[n], indent=4)
             else:
                 print('    "' + str(n) + '" : "' + str(json1[n]) + '"')
     return
 
+def file_match(file1, file2):
+    if type(file1) == list and type(file2) == list:
+        list_diff(file1, file2)
+    elif type(file1) == dict and type(file2) == dict:
+        dict_diff(file1, file2)
+    return
+
+def type_check(json1, json2):
+    if type(json1) == type(json2):
+        print("Type Check: Match " + str(type(json1)))
+    elif type(json1) != type(json2):
+        print("Type Check: Failed (type must match) \n\n File#1: " + str(type(json1)) + "\n File#2: " + str(type(json2)))
+    return
 
 def main():
     file1 = get_json()
     file2 = get_json()
-    print_diff(file1, file2)
-
+    type_check(file1, file2)
+    file_match(file1, file2)
+    
 if __name__ == "__main__":
     main()
