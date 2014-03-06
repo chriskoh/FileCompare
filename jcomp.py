b3857da9fa29a8eb663faf29490804f57f2924ee
chriskoh <chris.koh90@gmail.com>
2014-03-05 19:58:34 -0800

    Added comments

--- jcomp.py
+++ jcomp.py
@@ -3,66 +3,68 @@
 import json
 import pprint
 
-def get_json():
-    file_name = input("Enter name of JSON File: ")
-    with open(file_name) as json_file:
-        json_data = json.load(json_file)
-        return json_data
+def get_file(): # Function: Open files
+    file_name = input("Enter name of JSON File: ") # Set .json file as "file_name"
+    with open(file_name) as json_file: # Open "file_name" as "json_file"
+        json_data = json.load(json_file) # Parse "json_file" as "json_data" | json objects -> python dict / json array -> python lists
+        return json_data 
 
-def list_diff(json1, json2):
-    for i in json1:
-        if i in json2:
+def list_diff(file1, file2): # Compare list, file 2 to file 1 and print diffrences
+    for i in file1:
+        if i in file2: # Value is in BOTH files
             print('     "' + str(i) + '"')
-        elif i not in json2:
+        elif i not in file2: # Value is missing in file 2
             print('-    "' + str(i) + '"')
-    for i in json2:
-        if i not in json1:
+    for i in file2:
+        if i not in file1: # Value is only in file 2
             print('+    "' + str(i) + '"')
 
-def dict_diff(json1, json2):
-    for n in json1:
-        if n not in json2:
+def dict_diff(file1, file2): # Compare dict, file 2 to file 1 and print diffrences
+    for n in file1: # Compare Keys
+        if n not in file2: # Key is missing in file 2
             print('-   "' + str(n) + '":')
-    for n in json2:
-        if n not in json1:
+    for n in file2:
+        if n not in file1: # Key is only in file 2
             print('+   "' + str(n) + '":')
             continue
-        if json2[n] != json1[n]:
-            if type(json2[n]) not in (dict, list):
-                print('-   "' + str(n) + '" : "' + str(json1[n]) + '"')
-                print('+   "' + str(n) + '" : "' + str(json2[n]) + '"')
+        if file2[n] != file1[n]: # Compare values, value is only in file 2
+            if type(file2[n]) not in (dict, list): # Check, file 2 value is not a dict or list
+                print('-   "' + str(n) + '" : "' + str(file1[n]) + '"')
+                print('+   "' + str(n) + '" : "' + str(file2[n]) + '"')
             else:
-                if type(json2[n]) == dict:
-                    print('    "' + str(n) + '" : {')
-                    dict_diff(json1[n], json2[n])
+                if type(file2[n]) == dict: # File 2 value is type dict
+                    print('    "' + str(n) + '" : {') # Print Key file 2
+                    dict_diff(file1[n], file2[n]) # Recursion, compare dict in file 2 to value in file 1
                     continue
         else:
-            if type(json1[n]) == dict:
+            if type(file1[n]) == dict: # Value in BOTH files, and is type dict
                 print('    "' + str(n) + '" : {')
-                pprint.pprint(json1[n], indent=4)
-            else:
-                print('    "' + str(n) + '" : "' + str(json1[n]) + '"')
+                pprint.pprint(file1[n], indent=4) # Pretty print file 1 dict
+            else: # Value in BOTH files, not dict or list
+                print('    "' + str(n) + '" : "' + str(file1[n]) + '"')
     return
 
-def file_match(file1, file2):
-    if type(file1) == list and type(file2) == list:
+def file_match(file1, file2): # Compare, file 1 and file 2
+    if type(file1) == list and type(file2) == list: # if BOTH elements are list
         list_diff(file1, file2)
-    elif type(file1) == dict and type(file2) == dict:
+    elif type(file1) == dict and type(file2) == dict: # if BOTH elements are dict
         dict_diff(file1, file2)
     return
 
-def type_check(json1, json2):
-    if type(json1) == type(json2):
-        print("Type Check: Match " + str(type(json1)))
-    elif type(json1) != type(json2):
-        print("Type Check: Failed (type must match) \n\n File#1: " + str(type(json1)) + "\n File#2: " + str(type(json2)))
-    return
+def type_check(file1, file2): # Check, both files are JSON object or JSON Array
+    if type(file1) == type(file2):
+        print("Type Check: Match " + str(type(file1)))
+        return True
+    elif type(file1) != type(file2):
+        print("Type Check: Failed (type must match) \n\n File#1: " + str(type(file1)) + "\n File#2: " + str(type(file2)))
+    return False
 
 def main():
-    file1 = get_json()
-    file2 = get_json()
-    type_check(file1, file2)
-    file_match(file1, file2)
+    file1 = get_file() # load files
+    file2 = get_file()
+    check = type_check(file1, file2) # check types
+    if check == True: 
+        file_match(file1, file2) # compare
     
 if __name__ == "__main__":
     main()
